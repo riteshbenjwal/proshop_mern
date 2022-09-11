4465eb6d0f19fd5543791b0673c0fa40f4f1df1f
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-09-11 19:57:06 +0530

    Cors added for solving proxy issue, Fetching product from database, custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,33 @@
-import express from "express"
-import dotenv from 'dotenv'
-import colors from 'colors'
-import products from "./data/products.js"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDb from "./config/db.js";
 
-
 dotenv.config();
-
 const app = express();
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 connectDb();
 
-app.get("/", (req, res) => {
-  res.send("APi is running");
-});
+//Product routes
+import productRoutes from "./routes/productRoutes.js";
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
+);
