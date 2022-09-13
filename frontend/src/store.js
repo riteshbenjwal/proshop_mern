56874b01c8224ffd64871ed07eabd5ab1674b621
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2022-09-13 15:48:35 +0530

    Login Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,14 +5,21 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 
+import { userLoginReducer } from "./reducers/userReducers";
+
 import { cartReducer } from "./reducers/cartReducers";
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 const middleware = [thunk];
 
@@ -21,6 +28,7 @@ const store = configureStore({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
   },
   preloadedState: initialState,
   middleware: middleware,
